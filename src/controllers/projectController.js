8d64dcf5e9c5b1b045a10cd4e6e5af5cbe648f54
Thiag0Andres <thiagopalacios@eng.ci.ufpb.br>
2020-02-14 03:44:42 -0300

    .

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const authMiddleware = require('../middlewares/auth')
 
 const router = express.Router();
 
+router.use(authMiddleware);
+
 router.get('/', (req, res) => {
-    res.send({ ok: true});
-});
+    res.send({ ok: true });
+}); 
 
 module.exports = app => app.use('/project', router);
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,3 +1,6 @@
+const jwt =  require('jsonwebtoken');
+const authConfig = require('../config/auth.json');
+
 module.exports = (req, res, next) => {
     const authHeader = req.headers.authorization;
 
@@ -9,7 +12,16 @@ module.exports = (req, res, next) => {
     if(!parts.length === 2)
         return res.status(401).send({ error: 'Token error' });
 
-    const[ scheme, ]
+    const[ scheme, token ] = parts;
 
+    if(!/^Bearer$^/i.test(scheme))
+        return res.status(401).send({ error: 'Token malformatted' });
+
+    jwt.verify(token, authConfig.secret, (err, decoded) => {
+        if (err) return res.status(401).send({ error: 'token invalid' });
+
+        req.userId = decoded.id;
+        return next();
+    });
 
 };
